04099abeebf9b7aaea131aef1bfe3dad7930afd8
Siebe Holvoet <55406650+SHolvoet@users.noreply.github.com>
2021-10-18 15:31:44 +0200

    ef met migraties en sqlite db

--- BlazorBlog/BlazorBlog/Client/Services/BlogService.cs
+++ BlazorBlog/BlazorBlog/Client/Services/BlogService.cs
@@ -2,28 +2,38 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace BlazorBlog.Client.Services
 {
     public class BlogService : IBlogService
     {
-        public List<BlogPost> Posts { get; set; } = new List<BlogPost>()
-        {
-            new BlogPost {Url = "new-tutorial", Title = "A new Tutorial about Blazor", Description = "This is a new tutorial showing you how to build a blog with blazor",
-            Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
-            new BlogPost{Url = "first-post", Title = "My first blog post", Description = "Hi! This is my new shiny blog. Enjoy!",
-            Content = "This is my beautiful short blog post. Isn't it nice?"}
-        };
+        private readonly HttpClient _http;
 
-        public List<BlogPost> GetBlogPosts()
+        public BlogService(HttpClient http)
         {
-            return Posts;
+            _http = http;
         }
 
-        public BlogPost GetBlogPostByUrl(string url)
+        public async Task<BlogPost> GetBlogPostByUrl(string url)
         {
-            return Posts.FirstOrDefault(p => p.Url.ToLower().Equals(url.ToLower()));
+            var result = await _http.GetAsync($"api/Blog/{url}");
+            if (result.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                var message = await result.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+                return new BlogPost { Title = message};
+            }
+            else
+            {
+                return await result.Content.ReadFromJsonAsync<BlogPost>();
+            }
+        }
+        public async Task<List<BlogPost>> GetBlogPosts()
+        {
+            return await _http.GetFromJsonAsync<List<BlogPost>>("api/Blog");
         }
     }
 }
