aea8e81090b761c4552584ed6213782deef5a581
Siebe Holvoet <55406650+SHolvoet@users.noreply.github.com>
2021-10-18 22:02:51 +0200

    form post

--- BlazorBlog/BlazorBlog/Shared/BlogPost.cs
+++ BlazorBlog/BlazorBlog/Shared/BlogPost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,22 @@ namespace BlazorBlog.Shared
     public class BlogPost
     {
         public int Id { get; set; }
+        
+        [Required, StringLength(20, ErrorMessage = "Please use only 20 characters")]
         public string Url { get; set; }
+        [Required]
         public string Title { get; set; }
+        
         public string Content { get; set; }
+       
         public string Description { get; set; }
+        
         public string Author { get; set; }
+        
         public DateTime DateCreated { get; set; } = DateTime.Now;
+        
         public bool IsPublished { get; set; } = true;
+        
         public bool IsDeleted { get; set; } = false;
     }
 }
